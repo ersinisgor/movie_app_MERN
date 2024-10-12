8c19c6affbbf46dd5bc4aade0fa7fef7e950a0e9
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 21:09:40 +0300

    Add TV show routes and controller

---
+++ backend/controllers/tvShow.controller.js
@@ -0,0 +1,95 @@
+import { fetchFromTMDB } from "../services/tmdb.services.js";
+
+export async function getTrendingTvShow(req, res) {
+  try {
+    const data = await fetchFromTMDB(
+      "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1"
+    );
+
+    const randomTvShow =
+      data.results[Math.floor(Math.random() * data.results?.length)];
+
+    res.json({ success: true, content: randomTvShow });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      errorText: error.message,
+    });
+  }
+}
+
+export async function getTvShowTrailers(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`
+    );
+
+    res.json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      errorText: error.message,
+    });
+  }
+}
+
+export async function getTvShowDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US`
+    );
+    res.status(200).json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      errorText: error.message,
+    });
+  }
+}
+
+export async function getSimilarTvShows(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/tv/${id}/similar?language=en-US&page=1`
+    );
+
+    res.status(200).json({ success: true, similar: data.results });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      errorText: error.message,
+    });
+  }
+}
+
+export async function getTvShowsByCategory(req, res) {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/tv/${category}?language=en-US&page=1`
+    );
+
+    res.status(200).json({ success: true, content: data.results });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      errorText: error.message,
+    });
+  }
+}
---
+++ backend/routes/tvShow.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getTrendingTvShow,
+  getTvShowTrailers,
+  getTvShowDetails,
+  getSimilarTvShows,
+  getTvShowsByCategory,
+} from "../controllers/tvShow.controller.js";
+
+const router = express.Router();
+
+router.get("/trending", getTrendingTvShow);
+router.get("/:id/trailers", getTvShowTrailers);
+router.get("/:id/details", getTvShowDetails);
+router.get("/:id/similar", getSimilarTvShows);
+router.get("/:category", getTvShowsByCategory);
+
+export default router;
