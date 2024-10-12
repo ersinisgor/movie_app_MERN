16bc9ee977025f0a8ce5c3dd175f0ceb44f95078
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 13:17:16 +0300

    Refactor logout controller to clear JWT cookie with logout function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -72,5 +72,11 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
-  res.send("Logout Route");
+  try {
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 }
