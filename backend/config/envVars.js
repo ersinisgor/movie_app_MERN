4b133c9bdd1f0c2608bfe1e8e191ef95d38f5868
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 11:41:19 +0300

    Refactor envVars.js and auth.controller.js, Create JWT and set cokkie

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -5,4 +5,6 @@ dotenv.config();
 export const ENV_VARS = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 };
