56666af889b7843068de25f7da74b4e1984536c7
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 20:26:22 +0300

    Refactor movie controller and routes to add movie details endpoint

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getTrendingMovie,
   getMovieTrailers,
+  getMovieDetails,
 } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
 
 export default router;
