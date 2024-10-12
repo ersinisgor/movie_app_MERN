91ea0b4b9a80bcf13684ff82aa58904dc1099ba4
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 22:32:20 +0300

    Refactor search controller to add search history functionality for search people

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,7 +1,10 @@
+import { User } from "../models/user.model.js";
+import { fetchFromTMDB } from "../services/tmdb.services.js";
+
 export async function searchPerson(req, res) {
   const { query } = req.params;
   try {
-    const response = await fetch(
+    const response = await fetchFromTMDB(
       `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`
     );
 
@@ -9,16 +12,26 @@ export async function searchPerson(req, res) {
       return res.status(404).send(null);
     }
 
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].profile_path,
+          title: response.results[0].name,
+          searchType: "person",
+          createdAt: new Date(),
+        },
+      },
+    });
+
     res.status(200).json({ success: true, content: response.results });
   } catch (error) {
     console.log("Error in searchPerson contoller: ", error.message);
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Internal server error",
-        errorText: error.message,
-      });
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      errorText: error.message,
+    });
   }
 }
 export async function searchMovie(req, res) {}
