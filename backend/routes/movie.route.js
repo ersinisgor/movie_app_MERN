fb2392c246d47dbb9103905de0935a21e16cb697
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 20:38:16 +0300

    Add endpoint to get movies by category

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -4,6 +4,7 @@ import {
   getMovieTrailers,
   getMovieDetails,
   getSimilarMovies,
+  getMoviesByCategory,
 } from "../controllers/movie.controller.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
 router.get("/:id/details", getMovieDetails);
 router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
 
 export default router;
