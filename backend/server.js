383ef37589addb96d915f63b95d2f6beca5f67ba
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-12 21:44:57 +0300

    Refactor server.js to add authentication middleware for prevent unauthorized fetching

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,19 @@ import tvShowRoutes from "./routes/tvShow.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv-show", tvShowRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv-show", protectRoute, tvShowRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
