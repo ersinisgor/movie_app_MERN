641f61d398ba93b1896d58206746b710a9be5011
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-14 14:06:27 +0300

    Create Login page UI design, Refactor eslint.config.js for single quote escape warning,

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = e => {
+    e.preventDefault();
+    console.log(email, username, password);
+  };
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
@@ -16,7 +24,7 @@ const SignUpPage = () => {
             Sign Up
           </h1>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSignUp}>
             <div>
               <label
                 htmlFor="email"
@@ -29,6 +37,8 @@ const SignUpPage = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="you@example.com"
                 id="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
 
@@ -44,6 +54,8 @@ const SignUpPage = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="johndoe"
                 id="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
               />
             </div>
 
@@ -59,6 +71,8 @@ const SignUpPage = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="●●●●●●●●"
                 id="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
 
@@ -69,7 +83,7 @@ const SignUpPage = () => {
           <div className="text-center text-gray-400">
             Already a member?{" "}
             <Link to={"/login"} className="text-red-500 hover:underline">
-              Sign in
+              Sign In
             </Link>
           </div>
         </div>
