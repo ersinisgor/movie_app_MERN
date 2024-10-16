6ee3b245ff3d4a12f9f0adce9bec199074bb7e8a
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-16 17:32:42 +0300

    Refactor App.jsx and add WatchPage route, Create WatchPage component and add fetch data functions

---
+++ frontend/src/pages/WatchPage.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useContentStore } from "../store/content";
+import axios from "axios";
+
+const WatchPage = () => {
+  const { id } = useParams();
+  const [trailers, setTrailers] = useState([]);
+  const [currentTrailerIdx, setCurrentTrailerIdx] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState({});
+  const [similarContent, setSimilarContent] = useState([]);
+  const { contentType } = useContentStore();
+
+  useEffect(() => {
+    const getTrailers = async () => {
+      try {
+        const res = await axios.get(`/api/v1/${contentType}/${id}/trailers`);
+        setTrailers(res.data.trailers);
+      } catch (error) {
+        if (error.message.includes("404")) {
+          setTrailers([]);
+        }
+      }
+    };
+
+    getTrailers();
+  }, [contentType, id]);
+
+  useEffect(() => {
+    const getSimilarContent = async () => {
+      try {
+        const res = await axios.get(`/api/v1/${contentType}/${id}/similar`);
+        setSimilarContent(res.data.similar);
+      } catch (error) {
+        if (error.message.includes("404")) {
+          setSimilarContent([]);
+        }
+      }
+    };
+
+    getSimilarContent();
+  }, [contentType, id]);
+
+  useEffect(() => {
+    const getContentDetails = async () => {
+      try {
+        const res = await axios.get(`/api/v1/${contentType}/${id}/details`);
+        setContent(res.data.content);
+      } catch (error) {
+        if (error.message.includes("404")) {
+          setContent(null);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getContentDetails();
+  }, [contentType, id]);
+
+  console.log("trailers", trailers);
+  console.log("similarContent", similarContent);
+  console.log("content", content);
+};
+export default WatchPage;
