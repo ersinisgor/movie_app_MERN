da65bd0bdb44fb7d8b55d3f0a8ca2787d99dcecb
Ersin Isgor <ersinisgor@hotmail.com>
2024-10-16 17:47:37 +0300

    Refactor WatchPage component and add navigation buttons for trailers

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
+import Navbar from "../components/Navbar";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const WatchPage = () => {
   const { id } = useParams();
@@ -59,8 +61,50 @@ const WatchPage = () => {
     getContentDetails();
   }, [contentType, id]);
 
-  console.log("trailers", trailers);
-  console.log("similarContent", similarContent);
-  console.log("content", content);
+  const handleNext = () => {
+    if (currentTrailerIdx < trailers.length - 1)
+      setCurrentTrailerIdx(currentTrailerIdx + 1);
+  };
+  const handlePrev = () => {
+    if (currentTrailerIdx > 0) setCurrentTrailerIdx(currentTrailerIdx - 1);
+  };
+
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <div className="mx-auto container px-4 py-8 h-full">
+        <Navbar />
+
+        {trailers.length > 0 && (
+          <div className="flex justify-between items-center mb-4">
+            <button
+              className={`
+							bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === 0 ? "opacity-50 cursor-not-allowed " : ""
+              }}
+							`}
+              disabled={currentTrailerIdx === 0}
+              onClick={handlePrev}
+            >
+              <ChevronLeft size={24} />
+            </button>
+
+            <button
+              className={`
+							bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === trailers.length - 1
+                  ? "opacity-50 cursor-not-allowed "
+                  : ""
+              }}
+							`}
+              disabled={currentTrailerIdx === trailers.length - 1}
+              onClick={handleNext}
+            >
+              <ChevronRight size={24} />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default WatchPage;
